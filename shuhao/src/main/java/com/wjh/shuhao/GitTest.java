96fd140f681950bdc53c5dbf123d513488d837c9
wjh <1098306230@qq.com>
2022-04-06 00:19:46 +0800

    second commit

---
+++ shuhao/src/main/java/com/wjh/shuhao/GitTest.java
@@ -0,0 +1,13 @@
+package com.wjh.shuhao;
+
+/**
+ * @Author: wjh
+ * @Date: 2022/4/6 0:18
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+
+}
